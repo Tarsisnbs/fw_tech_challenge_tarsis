1ddb07816e415b1c0801af4343d40885242538b3
tarsisnbs <tarsis.silva@ecomp.ufsm.br>
2026-04-03 11:08:49 -0300

    [feature]Starting ISR integration with parser using Freertos queues

--- firmware/src/fw_main.c
+++ firmware/src/fw_main.c
@@ -18,9 +18,19 @@
 #include "hal_uart.h"
 #include "hal_gpio.h"
 #include "parser.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
 
 #include <stddef.h>
 #include <stdint.h>
+
+#define UART_QUEUE_LENGTH 64
+
+static uint8_t uart_queue_storage[UART_QUEUE_LENGTH];
+static StaticQueue_t uart_queue_struct;
+
+static QueueHandle_t uart_rx_queue; 
 
 //function to send hex byte
 static void send_hex_byte(uint8_t byte) {
@@ -41,6 +51,13 @@ static raw_packet_t pkt;
 
 // Callback UART2 (byte a byte)
 static void uart2_rx_callback(uint8_t byte) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    xQueueSendFromISR(uart_rx_queue, &byte, &xHigherPriorityTaskWoken);
+
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+
+    /*
     if (parse_byte(&parser, byte, &pkt)) {
         
         hal_uart1_send_str("\r\n[OK] Packet received\r\n");
@@ -53,7 +70,35 @@ static void uart2_rx_callback(uint8_t byte) {
 
         hal_uart1_send_str("wait next packet\r\n> ");
     }
+        */
 }
+
+static void parser_task(void *arg){
+    uint8_t byte;
+
+    parser_init(&parser);
+
+    while (1){
+        if(xQueueReceive(uart_rx_queue, &byte, portMAX_DELAY)){
+            if (parse_byte(&parser, byte, &pkt)) {
+        
+                hal_uart1_send_str("\r\n[OK] Packet received\r\n");
+       
+                hal_uart1_send_str("Payload: ");
+
+                for (uint8_t i = 0; i < pkt.payload_len; i++) {
+                    send_hex_byte(pkt.payload[i]);
+                }
+
+            hal_uart1_send_str("wait next packet\r\n> ");
+            }
+        }
+    }
+    
+}
+
+static StackType_t parser_stack[256];
+static StackType_t parser_tcb;
 
 void fw_init(void) {
     // UART debug
@@ -65,6 +110,24 @@ void fw_init(void) {
     hal_uart1_send_str("Send raw bytes via UART2\r\n");
     hal_uart1_send_str("> ");
 
+    uart_rx_queue = xQueueCreateStatic(
+        UART_QUEUE_LENGTH,
+        sizeof(uint8_t),
+        uart_queue_storage,
+        &uart_queue_struct
+    );
+
+    xTaskCreateStatic(
+        parser_task,
+        "parser",
+        256,
+        NULL,
+        2,
+        parser_stack,
+        &parser_tcb
+    );
+
+    /*
     // Inicializa parser
     parser_init(&parser);
 
@@ -73,13 +136,11 @@ void fw_init(void) {
 
     // LEDs (opcional)
     hal_gpio_pa5_init();
-
+    */
 }
 void fw_run(void) {
-    /* TODO: Start FreeRTOS scheduler */
-    uint8_t byte;
+    vTaskStartScheduler();
 
-    while (1) {
-        for (volatile int i = 0; i < 100000; i++);
-    }
+    while (1);
+    
 }
