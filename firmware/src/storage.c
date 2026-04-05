01bcfa2cfa7d970b46400d2f819c091740ba6d33
tarsisnbs <tarsis.silva@ecomp.ufsm.br>
2026-04-04 23:11:03 -0300

    [feat]storage ring system in modules, but is nexessary clear the fw_main

---
+++ firmware/src/storage.c
@@ -0,0 +1,53 @@
+#include "storage.h"
+#include <string.h>
+
+static sensor_data_t sensor_vault[MAX_SENSORS];
+static SemaphoreHandle_t xVaultMutex = NULL;
+static StaticSemaphore_t xVaultMutexBuffer;
+
+void storage_init(void) {
+    // Initializes the Mutex statically.
+    xVaultMutex = xSemaphoreCreateMutexStatic(&xVaultMutexBuffer);
+    
+    // Clear the vault's memory.
+    memset(sensor_vault, 0, sizeof(sensor_vault));
+}
+
+bool storage_save_packet(uint8_t sensor_id, uint8_t *payload, uint8_t len) {
+    if (sensor_id >= MAX_SENSORS || payload == NULL || len > PAYLOAD_SIZE) {
+        return false;
+    }
+
+    // Try to get the Mutex (short timeout so as not to crash the Parser).
+    if (xSemaphoreTake(xVaultMutex, pdMS_TO_TICKS(10)) == pdTRUE) {
+        sensor_data_t *s = &sensor_vault[sensor_id];
+        
+        // Calculates the Ring Buffer Index
+        uint8_t slot = s->sample_count % RING_BUFFER_SIZE;
+
+        // Copy the data
+        memcpy(s->slots[slot], payload, len);
+        memcpy(s->last_payload, payload, len);
+        
+        s->sample_count++;
+        s->last_rx_tick = xTaskGetTickCount();
+        s->registered = true;
+
+        xSemaphoreGive(xVaultMutex);
+        return true;
+    }
+    return false;
+}
+
+sensor_data_t* storage_get_sensor(uint8_t sensor_id) {
+    if (sensor_id >= MAX_SENSORS) return NULL;
+    return &sensor_vault[sensor_id];
+}
+
+bool storage_lock(TickType_t xTicksToWait) {
+    return (xSemaphoreTake(xVaultMutex, xTicksToWait) == pdTRUE);
+}
+
+void storage_unlock(void) {
+    xSemaphoreGive(xVaultMutex);
+}
---
+++ firmware/src/storage.h
@@ -0,0 +1,37 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+#define MAX_SENSORS       4
+#define RING_BUFFER_SIZE  8
+#define PAYLOAD_SIZE      16
+
+typedef struct {
+    uint8_t    slots[RING_BUFFER_SIZE][PAYLOAD_SIZE];
+    uint8_t    last_payload[PAYLOAD_SIZE];
+    uint32_t   sample_count;
+    TickType_t last_rx_tick;
+    bool       registered;
+} sensor_data_t;
+
+/* Initialize the Mutex and clear the Vault.*/
+void storage_init(void);
+
+/* Save a new package coming from the Parser.*/
+bool storage_save_packet(uint8_t sensor_id, uint8_t *payload, uint8_t len);
+
+/* 
+Returns a pointer to the sensor data (for the shell to read) 
+IMPORTANT: Must be used between storage_lock() and storage_unlock 
+   */
+sensor_data_t* storage_get_sensor(uint8_t sensor_id);
+
+/* Access control functions for the Shell */
+bool storage_lock(TickType_t xTicksToWait);
+void storage_unlock(void);
+
+#endif
